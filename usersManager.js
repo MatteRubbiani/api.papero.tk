aea145276276707568be69734c01e4d058f44228
MatteRubbbiani <matte.rubbiani@gmail.com>
2021-01-30 11:29:03 +0100

    f

---
+++ usersManager.js
@@ -0,0 +1,16 @@
+let mysql = require('mysql');
+
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "satteo!Britain13a"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mydb", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+});
