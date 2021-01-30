632e10ea362b831e984d309b13bf243ab28ae3f2
root <root@mail.sheetcheat.xyz>
2021-01-30 10:01:16 +0000

    b

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ let mysql = require('mysql');
 
 let con = mysql.createConnection({
     host: "localhost",
-    user: "matteo",
+    user: "root",
     password: "satteo!Britain13a"
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
 });
