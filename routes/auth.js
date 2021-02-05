cd167508660e6929bf6880d66b9dfbfb103abe70
MatteRubbbiani <matte.rubbiani@gmail.com>
2021-02-05 11:10:08 +0100

    H

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,7 @@ router.get(
     (req, res) =>{
         //res.send(req.cookies)
         //res.send(get_cookies(req)['from_location'])
-        //res.(decodeURI(get_cookies(req)['from_location']))
+        res.send(decodeURI(get_cookies(req)['from_location']))
         res.redirect("http://papero.tk")
     }
 )
