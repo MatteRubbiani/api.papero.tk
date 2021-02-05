f03a40fa42bab7fb74b1645a06a629635e47f438
MatteRubbbiani <matte.rubbiani@gmail.com>
2021-02-05 17:48:41 +0100

    H

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
 
 //Passport Middleware
 app.use(passport.initialize())
-app.use(passport.session({maxAge: 365 * 24 * 60 * 60 * 1000}))
+app.use(passport.session())
 
 //Cookies Middleware
 app.use(cookieParser("abcde"));
