89671b0102a48a251ade8704f657c866f8d59232
matteo <matte.rubbiani@gmail.com>
2021-02-10 17:04:18 +0100

    h

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router()
 
-router.get("/get_info", (req, res) =>{
-    let googleSignedIn = true
-    if (req.user) googleSignedIn = true //...
-    if (!req.user) {
-        res.send(null)
-        return null
-    }
+const get_cookies = function(request) {
+    let cookies = {};
+    request.headers && request.headers.cookie.split(';').forEach(function(cookie) {
+        let parts = cookie.match(/(.*?)=(.*)$/)
+        cookies[ parts[1].trim() ] = (parts[2] || '').trim();
+    });
+    return cookies;
+};
 
+router.get("/get_info", (req, res) =>{
+    let googleSignedIn = false
+    if (req.user) googleSignedIn = true //...
+    let username
+    if (!req.user) {
+        username = get_cookies(req)["username"]
+    }else{
+        username = req.user.firstName
+    }
     res.send({
-        username: req.user.firstName,
+        username: username,
         google_signed_in: googleSignedIn
     })
 })
