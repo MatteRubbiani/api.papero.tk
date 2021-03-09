8b533bda6c4a77bd06d1fd32bf89f99b1aa0a397
matteo <matte.rubbiani@gmail.com>
2021-03-09 10:35:50 +0100

    H

--- models/User.js
+++ models/User.js
@@ -9,17 +9,6 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String
-    },
     createdAt:{
         type: Date,
         default: Date.now
