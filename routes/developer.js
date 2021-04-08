a7aef61d2509ed7a7f33af944086c45eb461e60f
matteo <matte.rubbiani@gmail.com>
2021-04-08 08:38:37 +0200

    H

--- routes/developer.js
+++ routes/developer.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYxNzgyMDEwNCwiaWF0IjoxNjE3ODIwMTA0fQ.NercB9qr1qr3qNiKHXB9rPFxosYLnZMA5bbFtjD8jpE"
 
 router.get("/login", (req, res) =>{
-    res.cookie('developer-token', req.user.firstName, { maxAge: 2592000000 * 12});
+    res.cookie('developer-token', token, { maxAge: 2592000000 * 12});
     res.send("you are now a papero developer")
 })
 
